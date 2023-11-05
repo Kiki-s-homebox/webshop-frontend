569cfe64e81cb3567ab508f41d38650534e47865
gaurabmahat <gaurab.mahat.work@gmail.com>
2023-11-05 15:31:34 +0200

    made the blogs card responsive

--- app/components/Blogs/BlogCard.jsx
+++ app/components/Blogs/BlogCard.jsx
@@ -1,10 +1,21 @@
 import './blogCard.css';
 
 const BlogCard = ({image, title, body}) => {
+  const imageUrl = image.url;
+  const altText = image.altText;
+  // const contentHtmlText = body.substring(0, 200);
+  // console.log("contentHtmlText: ", contentHtmlText)
+  const contentHtmlText =
+    "If you're planning to move to Finland, one of the most important aspects to consider is finding suitable housing. While the Finnish housing market may seem complex, with the right knowledge and gui";
+
   return (
     <div className="cardWrapper">
-      <img src={image.url} className="blog-card-image" alt={image.altText} />
-      <div className="blog-title">{title}</div>
+      <img src={imageUrl} className="blog-card-image" alt={altText} />
+      <div className="blog-card-title">{title}</div>
+      <div
+        dangerouslySetInnerHTML={{__html: contentHtmlText}}
+        className="blog-card-text"
+      />
     </div>
   );
 };
