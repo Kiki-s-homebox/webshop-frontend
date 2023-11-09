e7b617dd4f770320af21cc43e1551dcf69025a45
Samuli Tolvanen <samuli.tolvanen@tuni.fi>
2023-11-09 22:40:04 +0200

    Fix else option

--- app/tests/hooks/usePagination.test.js
+++ app/tests/hooks/usePagination.test.js
@@ -46,7 +46,6 @@ describe('usePagination', () => {
       siblingCount,
       currentPage,
     });
-    console.log(paginationRange);
     expect(paginationRange).toEqual([1, -1, 4, 5, 6, 7, 8]);
   });
 
@@ -76,7 +75,6 @@ describe('usePagination', () => {
       siblingCount,
       currentPage,
     });
-    console.log(paginationRange);
-    expect(paginationRange).toEqual([1, 2, 3, 4, 5, -1, 10]);
+    expect(paginationRange).toEqual(undefined);
   });
 });
