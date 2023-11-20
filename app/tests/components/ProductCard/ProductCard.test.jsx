350fe5798870f50d1b724b7362db55ea1ed70d69
Roope <roope.salento@gmail.com>
2023-11-20 10:54:17 +0200

    test file improvements

--- app/tests/components/ProductCard/ProductCard.test.jsx
+++ app/tests/components/ProductCard/ProductCard.test.jsx
@@ -1,57 +1,57 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import ProductCard from '../../../components/ProductCard/ProductCard';
 
 jest.mock('@shopify/hydrogen', () => {
-    const Money = jest.fn(() => <p>Mocked Money</p>);
-    const Image = jest.fn(() => <p>Mocked Image</p>);
-    return {
-        Money,
-        Image
-    };
+  const Money = jest.fn(() => <p>Mocked Money</p>);
+  const Image = jest.fn(() => <p>Mocked Image</p>);
+  return {
+    Money,
+    Image,
+  };
 });
 
 jest.mock('@remix-run/react', () => {
-    const useNavigate = jest.fn(() => jest.fn());
-    return {
-      useNavigate,
-    };
-  });
+  const useNavigate = jest.fn(() => jest.fn());
+  return {
+    useNavigate,
+  };
+});
 
 describe('ProductCard', () => {
-    const mockProduct = {
-        id: 'id',
-        title: 'title',
-        handle: 'handle',
-        availableForSale: true,
-        createdAt: 'createdAt',
-        priceRange: {
-            maxVariantPrice: {
-                amount: 'amount',
-                currencyCode: 'currencyCode',
-            },
-            minVariantPrice: {
-                amount: 'amount',
-                currencyCode: 'currencyCode',
-            },
+  const mockProduct = {
+    id: 'id',
+    title: 'title',
+    handle: 'handle',
+    availableForSale: true,
+    createdAt: 'createdAt',
+    priceRange: {
+      maxVariantPrice: {
+        amount: 'amount',
+        currencyCode: 'currencyCode',
+      },
+      minVariantPrice: {
+        amount: 'amount',
+        currencyCode: 'currencyCode',
+      },
+    },
+    images: {
+      nodes: [
+        {
+          id: 'id',
+          url: 'url',
+          altText: 'altText',
+          width: 1,
         },
-        images: {
-            nodes: [
-                    {
-                        id: 'id',
-                        url: 'url',
-                        altText: 'altText',
-                        width: 1,
-                    }
-                   ]
-                }
-    };
+      ],
+    },
+  };
 
-    it('should render correctly with correct information', () => {
-        render(<ProductCard product={mockProduct} />);
-        // Assert that the product title is rendered
-        expect(screen.getByText('title')).toBeInTheDocument();
-        // Assert that the image alt text is rendered
-        expect(screen.getByAltText('altText')).toBeInTheDocument();
-    });
-})
+  it('should render correctly with correct information', () => {
+    render(<ProductCard product={mockProduct} />);
+    // Assert that the product title is rendered
+    expect(screen.getByText('title')).toBeInTheDocument();
+    // Assert that the image alt text is rendered
+    expect(screen.getByAltText('altText')).toBeInTheDocument();
+  });
+});
