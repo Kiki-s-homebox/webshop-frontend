f95ad76f6a4fdf7fa1673319325e30639362f0c8
cxanng <theanh090400@gmail.com>
2023-12-02 22:17:13 +0200

    Modify and add products fragments for recommended products and bestsellers, replace banner image

--- app/components/MainPage/MainPage.jsx
+++ app/components/MainPage/MainPage.jsx
@@ -1,15 +1,27 @@
 import MainPageBlogPosts from './MainPageBlogPosts';
-import MainPageRecommendedProducts from './MainPageRecommendedProducts';
-import image from '../../../public/bannersketch3.png';
+import image from '../../../public/bannersketch4.png';
 import './mainPage.css';
+import MainPageProductFragment from './MainPageProductFragment';
 
-const MainPage = ({data}) => {
-  const articles = data.nodes[0]?.articles;
+const MainPage = ({recommendedProducts, blogs, bestSellers}) => {
+  const articles = blogs?.articles;
 
   return (
-    <div>
+    <div className="main-page">
       <img src={image} className="mainPage-image" alt="Header" />
-      <MainPageRecommendedProducts products={data.recommendedProducts} />
+      {recommendedProducts && (
+        <MainPageProductFragment
+          title="Best deals"
+          subtitle="Get started with our kits"
+          products={recommendedProducts}
+        />
+      )}
+      {bestSellers && (
+        <MainPageProductFragment
+          title="Our bestsellers"
+          products={bestSellers}
+        />
+      )}
       {articles && <MainPageBlogPosts articles={articles} />}
     </div>
   );
