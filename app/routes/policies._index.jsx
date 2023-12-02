ed280fc1a5f708689d7b75853f0274eff25275a5
NhiTran <nhi.tran@futurice.com>
2023-12-02 19:00:03 +0200

    fix: lint bugs

--- app/routes/policies._index.jsx
+++ app/routes/policies._index.jsx
@@ -1,5 +1,5 @@
 import {json} from '@shopify/remix-oxygen';
-import {useLoaderData, Link} from '@remix-run/react';
+import {useLoaderData} from '@remix-run/react';
 
 import PoliciesMainPage from '~/components/Policies/PoliciesMainPage';
 
