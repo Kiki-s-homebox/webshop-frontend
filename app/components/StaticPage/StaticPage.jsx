f218a1b952b09d4a9f24bb4c00ada16305d223a3
Roope <roope.salento@gmail.com>
2023-12-06 14:24:54 +0200

    merged ContactUsPage.jsx to StaticPage.jsx since files without side effects are forbidden

--- app/components/StaticPage/StaticPage.jsx
+++ app/components/StaticPage/StaticPage.jsx
@@ -1,9 +1,33 @@
 import './StaticPage.css';
-import './ContactUsPage.jsx';
 
 const StaticPage = ({page}) => {
   if (page.handle === 'contact') {
-    return <ContactUsPage page={page} />;
+    return (
+      <div className="page">
+        <header>
+          <h1 className="page-title">{page.title}</h1>
+        </header>
+        <form id="contact-us-form">
+          <div class="form-group">
+            <input type="text" id="form-name" placeholder=" " />
+            <label for="form-name">Name</label>
+          </div>
+          <div class="form-group">
+            <input type="email" id="form-email" placeholder=" " />
+            <label for="form-email">Email</label>
+          </div>
+          <div class="form-group">
+            <input type="text" id="form-phonenumber" placeholder=" " />
+            <label for="form-phonenumber">Phone Number</label>
+          </div>
+          <div class="form-group">
+            <textarea id="form-comment" placeholder=" " />
+            <label for="form-comment">Comment</label>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   } else {
     return (
       <div className="page">
