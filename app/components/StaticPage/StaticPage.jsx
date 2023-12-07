6b10be23f33551a94c1b2ca66ce24f34c25ded60
Roope <roope.salento@gmail.com>
2023-12-07 10:27:02 +0200

    updated visuals for contact us page

--- app/components/StaticPage/StaticPage.jsx
+++ app/components/StaticPage/StaticPage.jsx
@@ -3,25 +3,25 @@ import './StaticPage.css';
 const StaticPage = ({page}) => {
   if (page.handle === 'contact') {
     return (
-      <div className="page">
+      <div className="contact-us">
         <header>
           <h1 className="page-title">{page.title}</h1>
         </header>
         <form id="contact-us-form">
           <div class="form-group">
-            <input type="text" id="form-name" placeholder=" " />
+            <input type="text" id="form-name" placeholder="" />
             <label for="form-name">Name</label>
           </div>
           <div class="form-group">
-            <input type="email" id="form-email" placeholder=" " />
+            <input type="email" id="form-email" placeholder="" />
             <label for="form-email">Email</label>
           </div>
           <div class="form-group">
-            <input type="text" id="form-phonenumber" placeholder=" " />
+            <input type="text" id="form-phonenumber" placeholder="" />
             <label for="form-phonenumber">Phone Number</label>
           </div>
           <div class="form-group">
-            <textarea id="form-comment" placeholder=" " />
+            <textarea id="form-comment" placeholder="" />
             <label for="form-comment">Comment</label>
           </div>
           <button type="submit">Submit</button>
