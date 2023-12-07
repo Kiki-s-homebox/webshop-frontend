5ccb456679c8b966e8c3198b4e89fd8e2b28f0ff
Roope <roope.salento@gmail.com>
2023-12-07 10:57:14 +0200

    updated to follow eslint rules

--- app/components/StaticPage/StaticPage.jsx
+++ app/components/StaticPage/StaticPage.jsx
@@ -8,21 +8,21 @@ const StaticPage = ({page}) => {
           <h1 className="page-title">{page.title}</h1>
         </header>
         <form id="contact-us-form">
-          <div class="form-group">
+          <div className="form-group">
             <input type="text" id="form-name" placeholder="" />
-            <label for="form-name">Name</label>
+            <label htmlFor="form-name">Name</label>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <input type="email" id="form-email" placeholder="" />
-            <label for="form-email">Email</label>
+            <label htmlFor="form-email">Email</label>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <input type="text" id="form-phonenumber" placeholder="" />
-            <label for="form-phonenumber">Phone Number</label>
+            <label htmlFor="form-phonenumber">Phone Number</label>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <textarea id="form-comment" placeholder="" />
-            <label for="form-comment">Comment</label>
+            <label htmlFor="form-comment">Comment</label>
           </div>
           <button type="submit">Submit</button>
         </form>
