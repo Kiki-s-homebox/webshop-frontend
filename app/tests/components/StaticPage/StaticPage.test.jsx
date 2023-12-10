25019c578eb83785ec3eb9508ebd97b190dddd70
Roope Salento <roope.salento@gmail.com>
2023-12-10 10:35:44 +0200

    fix eslint errors

--- app/tests/components/StaticPage/StaticPage.test.jsx
+++ app/tests/components/StaticPage/StaticPage.test.jsx
@@ -11,8 +11,8 @@ describe('Static Page component', () => {
   });
 
   it('renders contact us page when handle is contact', () => {
-    const page = {title: 'Contact us title', body: null, handle:'contact'};
-    render (<StaticPage page={page} />);
+    const page = {title: 'Contact us title', body: null, handle: 'contact'};
+    render(<StaticPage page={page} />);
 
     expect(screen.getByText('Name')).toBeInTheDocument();
     expect(screen.getByText('Email')).toBeInTheDocument();
@@ -20,14 +20,15 @@ describe('Static Page component', () => {
     expect(screen.getByText('Comment')).toBeInTheDocument();
   });
 
-  it('it does not render contact us when handle is not contact', () => {
-    const page = {title: 'Not Contact Us',
-                  body: 'Hello World',
-                  handle: 'not-contact'};
-    render (<StaticPage page={page} />);
-
+  it('does not render contact us when handle is not contact', () => {
+    const page = {
+      title: 'Not Contact Us',
+      body: 'Hello World',
+      handle: 'not-contact'
+    };
+    render(<StaticPage page={page} />);
+    
     expect(screen.getByText('Not Contact Us')).toBeInTheDocument();
     expect(screen.getByText('Hello World')).toBeInTheDocument();
   });
-
 });
